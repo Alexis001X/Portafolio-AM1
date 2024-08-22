14d985d65c38ccb402f4d2661e7923c84e9ded00
Alexis Mena <118840069+Alexis001X@users.noreply.github.com>
2024-08-22 13:52:43 -0500

    subida de portafolio

---
+++ assets/js/email.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('button-contact');
+
+document.getElementById('contact-form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_c4doand';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
